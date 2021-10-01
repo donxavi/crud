b0b34f96bd21ed0ed06a656d385319cc9bb3d77d
donxavi <donxavi4@gmail.com>
2021-10-01 09:40:09 +0530

    Components added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateComponent } from './components/create/create.component';
+import { EditComponent } from './components/edit/edit.component';
+import { HomeComponent } from './components/home/home.component';
+import { ViewComponent } from './components/view/view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'post/home', pathMatch:'full'},
+  {path:'post/home', component: HomeComponent},
+  {path:'post/:postId/view', component: ViewComponent},
+  {path:'post/create', component: CreateComponent},
+  {path:'post/:postId/edit', component: EditComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ViewComponent } from './components/view/view.component';
+import { CreateComponent } from './components/create/create.component';
+import { EditComponent } from './components/edit/edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ViewComponent,
+    CreateComponent,
+    EditComponent
   ],
   imports: [
     BrowserModule,
